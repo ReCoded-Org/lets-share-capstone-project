daf642771b4f248ae0b5882ff33b32d056ec819a
Amjad <amjadmaqsuma@gmail.com>
2022-07-20 19:59:32 +0300

    fix(header): npm run format was run right before commiting changes
    #1

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -78,24 +78,24 @@ export default function Header() {
                                         <div className='flex space-x-4'>
                                             {navigation.map((item) => (
                                                 <Link
-                                                key={item.name}
+                                                    key={item.name}
                                                     href={item.href}
-                                                    >
-                                                <a className={classNames(
-                                                        item.current
-                                                            ? "text-white"
-                                                            : "text-gray-300 hover:bg-slate-200 hover:text-white",
-                                                        "rounded-md px-3 py-2 text-sm font-medium"
-                                                    )}
-                                                    aria-current={
-                                                        item.current
-                                                            ? "page"
-                                                            : undefined
-                                                    }
-                                                    
                                                 >
-                                                    {item.name}
-                                                </a>
+                                                    <a
+                                                        className={classNames(
+                                                            item.current
+                                                                ? "text-white"
+                                                                : "text-gray-300 hover:bg-slate-200 hover:text-white",
+                                                            "rounded-md px-3 py-2 text-sm font-medium"
+                                                        )}
+                                                        aria-current={
+                                                            item.current
+                                                                ? "page"
+                                                                : undefined
+                                                        }
+                                                    >
+                                                        {item.name}
+                                                    </a>
                                                 </Link>
                                             ))}
                                         </div>
