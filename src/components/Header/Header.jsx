436ee5e80784d75179a44eb3be290466109d3ad2
Amjad <amjadmaqsuma@gmail.com>
2022-07-20 20:21:35 +0300

    style(header): trying to pass the prettier test

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,7 @@
 import { useTranslation } from "react-i18next";
 import Link from "next/link";
 import { Disclosure } from "@headlessui/react";
-import { MenuIcon, XIcon } from "@heroicons/react/outline";
+import { MenuIcon, XIcgion } from "@heroicons/react/outline";
 import UserMenu from "./UserMenu";
 import LangMenu from "./LangMenu";
 
