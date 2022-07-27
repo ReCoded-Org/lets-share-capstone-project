d6e1ba2f2855ad38363da26e1f2caf7a89f9ef4a
haniMuh <md.hani@hotmail.com>
2022-07-27 23:56:47 +0300

    refactor(component): edit input component
    edited input component to be more dynamic
    
    #22

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,68 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
-export default function Input(props) {
-    const title = props.title;
-    const name = props.name;
-    const email = props.email;
-    const password = props.password;
-    const phone = props.phone;
+export default function Input({ title, type, placeholder }) {
+    const [value, setValue] = useState("");
+    function handleChange(e) {
+        setValue(e.target.value);
+    }
 
     return (
         <div className='my-7 flex justify-center'>
             <div className='flex w-[80%] flex-col items-start justify-center md:w-[60%] xl:w-[40%]'>
                 <label className='mb-3 pl-3 text-xl font-semibold text-fontColor'>
-                    {title
-                        ? "Title"
-                        : name
-                        ? "Name"
-                        : email
-                        ? "Email"
-                        : password
-                        ? "Password"
-                        : phone
-                        ? "Phone"
-                        : "title"}
+                    {title}
                 </label>
                 <input
-                    type={
-                        title
-                            ? "text"
-                            : name
-                            ? "text"
-                            : email
-                            ? "email"
-                            : password
-                            ? "password"
-                            : phone
-                            ? "tel"
-                            : "text"
-                    }
-                    placeholder={
-                        title
-                            ? "Title"
-                            : name
-                            ? "Name"
-                            : email
-                            ? "Email"
-                            : password
-                            ? "Password"
-                            : phone
-                            ? "Phone"
-                            : "title"
-                    }
-                    name={
-                        title
-                            ? "text"
-                            : name
-                            ? "text"
-                            : email
-                            ? "email"
-                            : password
-                            ? "password"
-                            : phone
-                            ? "phone"
-                            : "text"
-                    }
+                    type={type}
+                    value={value}
+                    onChange={handleChange}
+                    placeholder={placeholder}
                     className='w-full rounded-lg border-2 border-primary shadow-md'
                 />
             </div>
