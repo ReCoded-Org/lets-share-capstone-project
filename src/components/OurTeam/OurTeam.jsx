64d9ace2217271a61736a233a0dd3f45cb706c0c
haniMuh <md.hani@hotmail.com>
2022-08-02 18:50:04 +0300

    refactor(about us page): refactor about us page
    put code in index file and edit our team cards style
    
    #34

--- src/components/OurTeam/OurTeam.jsx
+++ src/components/OurTeam/OurTeam.jsx
@@ -45,39 +45,35 @@ const profiles = [
 
 function OurTeam() {
     return (
-        <div className=''>
-            <h1 className=' py-5 text-center text-2xl font-bold '>Our Team</h1>
-            <div className=' grid-col-1   grid justify-items-center md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  '>
+        <div className='justify-self-center'>
+            <h1 className=' mb-5 py-5 text-center text-2xl font-bold '>
+                Our Team
+            </h1>
+            <div className='grid-col-1 mx-[7rem] grid justify-items-center md:grid-cols-2 lg:grid-cols-4'>
                 {profiles.map((profile, i) => (
-                    <div key={i}>
-                        <div className='   w-48   justify-items-center '>
-                            <div>
-                                <div className=' h-48 w-48 cursor-pointer  rounded-full bg-secondary shadow transition duration-300 ease-in-out hover:scale-110 '>
-                                    <Image
-                                        className='rounded-full'
-                                        src={profile.image}
-                                        alt='Lets-share-logo'
-                                        width={200}
-                                        height={200}
-                                    />
-                                </div>
-                                <div className=' p-2 text-center '>
-                                    <div className='flex justify-center gap-2 p-2 text-3xl'>
-                                        <Link href='{profile.Likedin}'>
-                                            <AiFillLinkedin className='cursor-pointer   hover:text-fontColor ' />
-                                        </Link>
-                                        <Link href='{profile.Github}'>
-                                            <AiFillGithub className='cursor-pointer   hover:text-fontColor' />
-                                        </Link>
-                                    </div>
-                                    <h2 className='font-bold'>
-                                        {profile.name}
-                                    </h2>
-                                    <p className='max-w-[200px]'>
-                                        {profile.description}
-                                    </p>
-                                </div>
+                    <div key={i} className=' my-6 w-48 justify-center'>
+                        <div className=' h-48 w-48 cursor-pointer rounded-full bg-secondary shadow transition duration-300 ease-in-out hover:scale-110 '>
+                            <Image
+                                className='rounded-full'
+                                src={profile.image}
+                                alt='Lets-share-logo'
+                                width={200}
+                                height={200}
+                            />
+                        </div>
+                        <div className=' p-2 text-center '>
+                            <div className='flex justify-center gap-2 p-2 text-3xl'>
+                                <Link href='{profile.Likedin}'>
+                                    <AiFillLinkedin className='cursor-pointer   hover:text-fontColor ' />
+                                </Link>
+                                <Link href='{profile.Github}'>
+                                    <AiFillGithub className='cursor-pointer   hover:text-fontColor' />
+                                </Link>
                             </div>
+                            <h2 className='font-bold'>{profile.name}</h2>
+                            <p className='max-w-[200px]'>
+                                {profile.description}
+                            </p>
                         </div>
                     </div>
                 ))}
--- src/pages/aboutus/index.jsx
+++ src/pages/aboutus/index.jsx
@@ -1,2 +1,35 @@
-import Aboutus from "./Aboutus";
-export default Aboutus;
+import React from "react";
+import OurTeam from "@/components/OurTeam";
+import Image from "next/image";
+import Layout from "@/components/layout/Layout";
+
+export default function Aboutus() {
+    return (
+        <Layout>
+            <div className='mt-[20px] flex flex-col items-center px-5 md:ml-20 md:h-[60vh] md:flex-row  '>
+                <div className=' relative flex h-[30rem] w-full flex-row items-center md:order-1  '>
+                    <Image
+                        src='/puzzels.svg'
+                        layout='fill'
+                        objectFit='contain'
+                        alt='/'
+                    />
+                </div>
+                <div className='relative mx-10  my-[30px] flex   h-[341px] w-[60%] flex-col items-center justify-center text-center md:items-start'>
+                    <h2 className='text-2xl font-bold text-fontColor'>
+                        Who we are
+                    </h2>
+                    <p className='text-2xll mb-8 py-5 sm:w-[100%] md:text-left md:text-xl'>
+                        Lorem, ipsum dolor sit amet consectetur adipisicing
+                        elit. Fugiat, iusto sed alias obcaecati veniam cumque
+                        nesciunt sint, possimus, adipisci animi mollitia quam?
+                        Pariatur, consequatur? Saepe illum mollitia repellendus
+                        quod perferendis? consectetur adipisicing elit. Fugiat,
+                        iusto sed alias obcaecati veniam cumque
+                    </p>
+                </div>
+            </div>
+            <OurTeam />
+        </Layout>
+    );
+}
