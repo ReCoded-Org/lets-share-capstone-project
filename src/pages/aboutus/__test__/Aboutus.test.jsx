6f0d45f6e1d92c8147d85de9d996f21739933c6e
haniMuh <md.hani@hotmail.com>
2022-08-02 12:17:41 +0300

    feat(about us page): about us page added
    about us page added
    
    feat #34

--- src/pages/aboutus/__test__/Aboutus.test.jsx
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import renderer from "react-test-renderer";
-import Aboutus from "../Aboutus";
-
-it("renders correctly", () => {
-    const tree = renderer.create(<Aboutus />).toJSON();
-    expect(tree).toMatchSnapshot();
-});
