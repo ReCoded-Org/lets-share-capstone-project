64a46cc3cda5148a9e90da260009d3d550b5e6ff
Amjad <amjadmaqsuma@gmail.com>
2022-08-15 23:09:57 +0300

    feat(blogs): blogs routing is done
    closes issue #45

--- src/components/BlogCard/__test__/card.test.jsx
+++ src/components/BlogCard/__test__/card.test.jsx
@@ -3,8 +3,14 @@
 import renderer from "react-test-renderer";
 
 import Card from "../card.jsx";
+const item = {
+    id: 1,
+    title: "My-Blog",
+    image: "/Photo.png",
+    imageAlt: "Sunset in the mountains",
+};
 
 it("renders correctly", () => {
-    const tree = renderer.create(<Card />).toJSON();
+    const tree = renderer.create(<Card item={item} />).toJSON();
     expect(tree).toMatchSnapshot();
 });
